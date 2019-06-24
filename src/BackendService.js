b9d06f595686316c7529a0131016ebc1b7cb5fa9
chong yang <chongyang1995@gmail.com>
2019-06-24 19:20:16 +0100

    add issue for xiao you

--- src/BackendService.js
+++ src/BackendService.js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 const submitUrl = "https://essay-helper.herokuapp.com/submit";
 const testUrl = "http://localhost:2999/submit";
+var fs = require('fs');
 
+var data = fs.readFileSync('App.js', 'utf8');
+console.log(data);
+
+fs.readFile('App.js', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 
 const postPdf = async (fileType, file) => {
   const url = testUrl;
@@ -11,13 +19,23 @@ const postPdf = async (fileType, file) => {
     Accept: "application/json"
   };
 
-  var data = new FormData();
-  data.append('file', file);
+  fs.readFile('demofile1.html', function(err, data) {
+    console.log(data)
+  });
 
-  axios
-    .post(url, data, { headers: headers })
-    .then(response => console.log("POST server success: ", response))
-    .catch(error => console.error("POST server error: ", error));
+  // var stream = fs.createReadStream()
+  // fs.readFile(__dirname + file, (err, data) => {
+  //   console.log(data)
+  // })
+
+  // var stream = fs.createReadStream(file);
+  // var data = new FormData();
+  // data.append('file', file);
+
+  // axios
+  //   .post(url, data, { headers: headers })
+  //   .then(response => console.log("POST server success: ", response))
+  //   .catch(error => console.error("POST server error: ", error));
 };
 
 export default postPdf;
